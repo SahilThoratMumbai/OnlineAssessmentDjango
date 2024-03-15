8eb02d9b56ef931d67dd227b181808e66073acad
Vaibhav wagh <vaibhavwagh393@gmail.com>
2024-03-15 16:49:51 +0530

    vaibhavs commit

--- collegeassessment/apps.py
+++ collegeassessment/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class CollegeassessmentConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "collegeassessment"
+
+#WAGH UNDE THORAT BHISE
